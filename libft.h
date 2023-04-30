9ac917eb96e000d2e2daeea6f2591a96e20be885
TIxtex <tixtex666@gmail.com>
2023-04-30 23:45:50 +0200

    fix

--- libft.h
+++ libft.h
@@ -126,6 +126,7 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
+void			ft_puterror(const char *error);
 /**	END_FT_PUT	**/
 
 #endif
